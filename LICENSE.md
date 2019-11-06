c88f463b5ece087f907fc7695142dd04bb1e3324
Korneliusz Buczkowski <korneliusz.buczkowski@capsilon.com>
2019-11-06 07:36:12 +0100

    update project to newest setup from electron-quick-start

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2018 Korneliusz Buczkowski
+Copyright (c) 2019 Korneliusz Buczkowski
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- js/controllers/trace-route-tab-controller.js
+++
@@ -1,8 +0,0 @@
-const TracertHandler = require('../tracert-handler');
-
-const traceRouteResultsEl = document.getElementById("trace-route-results");
-const traceTargetNameEl = document.getElementById("trace-target-name");
-const traceStopEl = document.getElementById("trace-route-stop");
-const traceStartEl = document.getElementById("trace-route-start");
-
-new TracertHandler(traceStartEl, traceStopEl, traceTargetNameEl, traceRouteResultsEl);
